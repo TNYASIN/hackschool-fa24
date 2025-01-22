aa84cbd3626cee49a24e3ec44b98ff19b5eeec90
Tanya Sinha <tanyasinha301@gmail.com>
2025-01-22 07:08:56 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,1 @@
-import axios from 'axios';
 
-const serverURL = 'http://localhost:3000';
-
-const TravelAPi = {
-    getTrip: () => axios.get(`${serverURL}/api/trip`),
-    getActivity: () => axios.get(`${serverURL}/api/activity`),
-    postTrip: (payload) => axios.post(`${serverURL}/api/trip`, payload),
-    postActivity: (payload) => axios.post(`${serverURL}/api/activity`, payload),
-};
-
-export default TravelAPi;
